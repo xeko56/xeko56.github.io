d6cc8909150578d8d5a2a4ab51f6e2a4bc41d8b0
Tan Nguyen <tanmatic.tm@gmail.com>
2025-06-03 23:36:52 +0200

    Update banner

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,14 @@
 import { defineConfig } from 'astro/config';
 import vue from '@astrojs/vue';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-	// Enable Vue to support Vue components.
-	integrations: [vue()],
+  // Enable Vue to support Vue components.
+  integrations: [vue()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
