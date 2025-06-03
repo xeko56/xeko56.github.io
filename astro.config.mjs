90e2ffd3276568cfcd2d59d36397252841c84b1c
Tan Nguyen <tanmatic.tm@gmail.com>
2025-06-03 23:41:32 +0200

    Update for deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://xeko56.github.io',
   // Enable Vue to support Vue components.
   integrations: [vue()],
 
